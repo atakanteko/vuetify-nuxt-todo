25959cea2300e45b75f5a1568c6740f8581e0232
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-02-16 22:51:00 +0300

    set bg color and text decoration line through if checkbox selected moreover if it is checked set done status true or viceversa

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,21 @@
   <div class='home pa-6'>
     <v-list class='pt-0' flat>
       <v-list-item-group multiple>
-        <div v-for='(item,index) in this.tasks' :key='index'>
-          <v-list-item>
-            <template v-slot:default='{ active, }'>
+        <div v-for='(item,index) in this.tasks'
+             :key='index'
+             :class='{ shadow:item.done }'>
+          <v-list-item @click='toggleDone(item.id)'>
+            <template v-slot:default>
               <v-list-item-action>
                 <v-checkbox
-                  :input-value='active'
-                  color='primary'
+                  :input-value='item.done'
                 ></v-checkbox>
               </v-list-item-action>
               <v-list-item-content>
-                <v-list-item-title>{{ item.title }}</v-list-item-title>
+                <v-list-item-title
+                  :class='{"text-decoration-line-through":item.done}'>
+                  {{ item.title }}
+                </v-list-item-title>
               </v-list-item-content>
             </template>
           </v-list-item>
@@ -28,12 +32,38 @@ export default {
   name: 'IndexPage',
   data() {
     return {
+      checkedNames: [],
       tasks: [
-        { id: 1, title: 'Wake Up' },
-        { id: 2, title: 'Get a Cup of Tea' },
-        { id: 3, title: 'Drink Tea' },
+        {
+          id: 1,
+          title: 'Wake Up',
+          done: false,
+        },
+        {
+          id: 2,
+          title: 'Get a Cup of Tea',
+          done: false,
+        },
+        {
+          id: 3,
+          title: 'Drink Tea',
+          done: false,
+        },
       ],
     };
   },
+  methods: {
+    toggleDone(taskId) {
+      const task = this.tasks.filter((item) => item.id === taskId)[0];
+      task.done = !task.done;
+    },
+  },
 };
 </script>
+<style lang='scss' scoped>
+@import 'assets/variables';
+
+.shadow {
+  background-color: $light-blue;
+}
+</style>
