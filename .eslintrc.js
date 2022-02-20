69f905e32546410940c49ba5c603c43f3de8802f
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-02-20 16:04:59 +0300

    change lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,25 +4,57 @@ module.exports = {
     browser: true,
     node: true,
   },
-  settings: {
-    'import/core-modules': ['vuetify', 'vuetify/es5/util/colors'],
-    'import/resolver': {
-      alias: {
-        map: [['@', '.']],
-        extensions: ['.vue', '.js'],
-      },
-    },
-  },
-  parserOptions: {
-    parser: '@babel/eslint-parser',
-    requireConfigFile: false,
-  },
-  extends: ['plugin:vue/essential', '@vue/airbnb'],
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'airbnb-base',
+  ],
   plugins: [],
   // add your custom rules here
   rules: {
-    'vue/multi-word-component-names': ['error', {
-      ignores: ['default'],
+    'arrow-parens': [
+      2,
+      'as-needed',
+    ],
+    'vue/multi-word-component-names': 0,
+    'import/prefer-default-export': 'off',
+    'import/no-unresolved': 'off',
+    'no-param-reassign': 'off',
+    'no-console': 'off',
+    'no-unused-expressions': 'off',
+    'global-require': 'off',
+    'import/extensions': 'off',
+    'func-names': 'off',
+    'no-plusplus': 'off',
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: true,
+      },
+    ],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [
+          '_id',
+          '_doc',
+        ],
+      },
+    ],
+    'operator-linebreak': 'off',
+    'brace-style': [
+      'error',
+      '1tbs',
+    ],
+    indent: [
+      'error',
+      2,
+    ],
+    'max-len': ['error', {
+      code: 160,
+      tabWidth: 2,
+      ignorePattern: 'd=\'([\\s\\S]*?)\'',
     }],
+    'no-shadow': ['error', { allow: ['state'] }],
   },
 };
