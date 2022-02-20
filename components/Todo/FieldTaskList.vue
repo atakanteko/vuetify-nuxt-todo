e8cda0fcd657da08f38bee2718b303205234ab4a
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-02-20 17:26:19 +0300

    add single task component

--- components/Todo/FieldTaskList.vue
+++ components/Todo/FieldTaskList.vue
@@ -1,66 +1,29 @@
 <template>
   <v-list class="pt-0" flat>
     <v-list-item-group multiple>
-      <div
+      <SingleTask
         v-for="(item,index) in getTasks"
         :key="index"
         :class="{ shadow:item.done }"
-      >
-        <v-list-item @click="toggleDone(item.id)">
-          <template #default>
-            <v-list-item-action>
-              <v-checkbox
-                :input-value="item.done"
-              />
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title
-                :class="{'`text-decoration-line-through`':item.done}"
-              >
-                {{ item.title }}
-              </v-list-item-title>
-            </v-list-item-content>
-            <v-list-item-action>
-              <v-btn
-                icon
-                @click.stop="removeTodo(item.id)"
-              >
-                <v-icon color="primary">
-                  {{ icons.delete }}
-                </v-icon>
-              </v-btn>
-            </v-list-item-action>
-          </template>
-        </v-list-item>
-        <v-divider />
-      </div>
+        :item="item"
+      />
     </v-list-item-group>
   </v-list>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import SingleTask from './SingleTask';
 
 export default {
   name: 'FieldTaskList',
+  components: {
+    SingleTask,
+  },
   props: {
     getTasks: {
       type: Array,
       default: null,
     },
-  },
-  data() {
-    return {
-      icons: {
-        delete: 'mdi-delete',
-      },
-    };
-  },
-  methods: {
-    ...mapActions({
-      removeTodo: 'store/removeTodo',
-      toggleDone: 'store/toggleDone',
-    }),
   },
 };
 </script>
---
+++ components/Todo/SingleTask.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <v-list-item @click="toggleDone(item.id)">
+      <template #default>
+        <v-list-item-action>
+          <v-checkbox
+            :input-value="item.done"
+          />
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title
+            :class="{'`text-decoration-line-through`':item.done}"
+          >
+            {{ item.title }}
+          </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn
+            icon
+            @click.stop="removeTodo(item.id)"
+          >
+            <v-icon color="primary">
+              {{ icons.delete }}
+            </v-icon>
+          </v-btn>
+        </v-list-item-action>
+      </template>
+    </v-list-item>
+    <v-divider />
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'SingleTask',
+  props: {
+    item: {
+      type: Object,
+      default: null,
+    },
+  },
+  data() {
+    return {
+      icons: {
+        delete: 'mdi-delete',
+      },
+    };
+  },
+  methods: {
+    ...mapActions({
+      removeTodo: 'store/removeTodo',
+      toggleDone: 'store/toggleDone',
+    }),
+  },
+};
+</script>
+<style scoped></style>
