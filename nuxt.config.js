9ecb39371ae920326ca8fba4198050f8fa92fe5c
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-02-20 16:58:53 +0300

    change theme to light

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors';
-
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
@@ -48,16 +46,18 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
+      dark: false,
+      theme: {
+        themes: {
+          light: {
+            primary: '#00bcd4',
+            secondary: '#8bc34a',
+            accent: '#3f51b5',
+            error: '#e91e63',
+            warning: '#ffeb3b',
+            info: '#2196f3',
+            success: '#4caf50',
+          },
         },
       },
     },
