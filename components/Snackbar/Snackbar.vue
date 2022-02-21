79da866378a0b1f2521937f2954d0d471c298ea8
atakan.tekoglu <atakan.tekoglu@modanisa.com>
2022-02-21 21:26:34 +0300

    add snackbar component

---
+++ components/Snackbar/Snackbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="text-center ma-2">
+    <v-snackbar
+      v-model="snackbar"
+    >
+      {{ text }}
+
+      <template #action="{ attrs }">
+        <v-btn
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    snackbar: false,
+    text: 'Hello, I\'m a snackbar',
+  }),
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,12 @@
 <template>
-  <v-app id='inspire'>
+  <v-app id="inspire">
     <v-navigation-drawer
-      v-model='drawer'
+      v-model="drawer"
       app
     >
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class='text-h6'>
+          <v-list-item-title class="text-h6">
             Vuetify Todo App
           </v-list-item-title>
           <v-list-item-subtitle>
@@ -15,16 +15,16 @@
         </v-list-item-content>
       </v-list-item>
 
-      <v-divider></v-divider>
+      <v-divider />
 
       <v-list
         dense
         nav
       >
         <v-list-item
-          v-for='item in items'
-          :key='item.title'
-          :to='item.link'
+          v-for="item in items"
+          :key="item.title"
+          :to="item.link"
           link
         >
           <v-list-item-icon>
@@ -39,24 +39,24 @@
     </v-navigation-drawer>
 
     <v-app-bar
-      :src='require(`~/assets/images/universe.jpg`)'
+      :src="require(`~/assets/images/universe.jpg`)"
       app
-      color='primary'
+      color="primary"
       dark
       prominent
     >
-      <template v-slot:img='{ props }'>
+      <template #img="{ props }">
         <v-img
-          gradient='to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)'
-          v-bind='props'
-        ></v-img>
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+          v-bind="props"
+        />
       </template>
 
-      <v-app-bar-nav-icon @click='drawer = !drawer'></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer = !drawer" />
 
       <v-app-bar-title>Vuetify Todo</v-app-bar-title>
 
-      <v-spacer></v-spacer>
+      <v-spacer />
 
       <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
@@ -73,12 +73,18 @@
 
     <v-main>
       <Nuxt />
+      <Snackbar />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import Snackbar from '../components/Snackbar/Snackbar';
+
 export default {
+  components: {
+    Snackbar,
+  },
   data() {
     return {
       drawer: null,
